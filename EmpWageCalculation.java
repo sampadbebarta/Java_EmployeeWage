a208acac30654c1b4b1369365d7eb4d107b78b51
Sampad <bebartasampad@gmail.com>
2021-01-21 15:06:05 +0530

    Program of Full Time, Part Time Employee and Wages

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,20 +1,24 @@
-public class EmpWageCalculation {
-	public static void main(String args[]) {
-		//Constants
-		final int IS_FULL_TIME = 1;
-		final int EMP_RATE_PER_HOUR = 20;
-		//variables
-		int empHrs =  0;
-		int empWage = 0;
-		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
-			empHrs = 8;
-		}
-		else
-			System.out.println("Employee is Absent");
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: "+empWage);
-	}
+public class EmpWageCalculation
+{
+    public static void main( String[] args )
+    {
+    	final int IS_FULL_TIME = 1;
+    	final int IS_PART_TIME= 2;
+    	final int PER_HOUR_WAGE = 20;
+    	int noOfHrs = 0;
+    	int empWage=0;
+    	double empCheck = Math.floor(Math.random() * 10)%3;
+    	if(IS_FULL_TIME == empCheck) {
+    		noOfHrs = 8;
+    		System.out.println("Employee is Full Time");
+    	}
+    	else if(IS_PART_TIME == empCheck) {
+    		noOfHrs = 4;
+    		System.out.println("Employee is Part Time");
+    	}
+    	else
+    		System.out.println("Employee is Absent");
+    	empWage= noOfHrs*PER_HOUR_WAGE;
+        System.out.println( "Employee's wage for the day :" + empWage);
+    }
 }
