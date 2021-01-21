26f733a3a2841728d1c51f6ae0bfafac55e42a05
Sampad <bebartasampad@gmail.com>
2021-01-21 15:45:34 +0530

    Program of Employee's Wage till a condition

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -5,22 +5,32 @@ public class EmpWageCalculation
     	final int IS_FULL_TIME = 1;
     	final int IS_PART_TIME= 2;
     	final int PER_HOUR_WAGE = 20;
+    	final int MAX_WORKING_HRS = 100;
+    	final int MAX_WORKING_DAYS = 15;
+    	int totalWorkingHrs=0;
     	int totalWage=0;
-    	for(int i=0;i<20;i++) {
+    	int days=0;
+    	while(totalWorkingHrs< MAX_WORKING_HRS && days < MAX_WORKING_DAYS) {
     		int noOfHrs = 0;
     		final int empCheck = (int)Math.floor(Math.random() * 10)%3;
     		switch(empCheck) {
     		case IS_FULL_TIME: 	noOfHrs = 8;
+    						days += 1;
     						System.out.println("Employee is Full Time");
     						break;
     		case IS_PART_TIME:	noOfHrs = 4;
+    						days += 1;
     						System.out.println("Employee is Part Time");
     						break;
     		default:			System.out.println("Employee is Absent");
     		}
+    		totalWorkingHrs += noOfHrs;
     		final int empWage= noOfHrs*PER_HOUR_WAGE;
     		totalWage+=empWage;
     	}
-        System.out.println( "Employee's wage for the month :" + totalWage);
+    	if(totalWorkingHrs>=100)
+    		System.out.println( "Employee's wage for completing " + MAX_WORKING_HRS + " or more Hrs " + totalWage);
+    	else
+    		System.out.println("Employee's wage after working for " + MAX_WORKING_DAYS + " or more days, his/her wage is " + totalWage);
     }
 }
