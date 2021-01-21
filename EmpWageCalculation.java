0af41013e091c0622f7d6c39d88471b88bd42ed8
Sampad <bebartasampad@gmail.com>
2021-01-21 15:13:05 +0530

    Program of Full Time, Part Time Employee and Wages using Switch Case

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -7,17 +7,16 @@ public class EmpWageCalculation
     	final int PER_HOUR_WAGE = 20;
     	int noOfHrs = 0;
     	int empWage=0;
-    	double empCheck = Math.floor(Math.random() * 10)%3;
-    	if(IS_FULL_TIME == empCheck) {
-    		noOfHrs = 8;
-    		System.out.println("Employee is Full Time");
+    	final int empCheck = (int)Math.floor(Math.random() * 10)%3;
+    	switch(empCheck) {
+    	case IS_FULL_TIME: 	noOfHrs = 8;
+    						System.out.println("Employee is Full Time");
+    						break;
+    	case IS_PART_TIME:	noOfHrs = 4;
+    						System.out.println("Employee is Part Time");
+    						break;
+    	default:			System.out.println("Employee is Absent");
     	}
-    	else if(IS_PART_TIME == empCheck) {
-    		noOfHrs = 4;
-    		System.out.println("Employee is Part Time");
-    	}
-    	else
-    		System.out.println("Employee is Absent");
     	empWage= noOfHrs*PER_HOUR_WAGE;
         System.out.println( "Employee's wage for the day :" + empWage);
     }
