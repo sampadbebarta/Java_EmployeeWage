ab0220a615f670dae1dffac351512fea2a1e9551
Sampad <bebartasampad@gmail.com>
2021-01-21 14:05:59 +0530

    Program of Employee Wage Calculation

---
+++ EmpWageCalculation.java
@@ -0,0 +1,13 @@
+public class EmpWageCalculation
+{
+    public static void main( String[] args )
+    {
+		System.out.println( "Welcome to Employee Wage Computation");
+    	int IS_FULL_TIME=1;
+    	double empCheck = Math.floor(Math.random() * 10)%2;
+    	if(IS_FULL_TIME == empCheck)
+    		System.out.println("Employee is Present");
+    	else
+    		System.out.println("Employee is Absent");
+    }
+}
