e245d269a5fae88a823aa8af29226ef0f744ca4d
Sampad <bebartasampad@gmail.com>
2021-01-21 15:38:58 +0530

    Program of Employee's Wage for a month

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -5,19 +5,22 @@ public class EmpWageCalculation
     	final int IS_FULL_TIME = 1;
     	final int IS_PART_TIME= 2;
     	final int PER_HOUR_WAGE = 20;
-    	int noOfHrs = 0;
-    	int empWage=0;
-    	final int empCheck = (int)Math.floor(Math.random() * 10)%3;
-    	switch(empCheck) {
-    	case IS_FULL_TIME: 	noOfHrs = 8;
+    	int totalWage=0;
+    	for(int i=0;i<20;i++) {
+    		int noOfHrs = 0;
+    		final int empCheck = (int)Math.floor(Math.random() * 10)%3;
+    		switch(empCheck) {
+    		case IS_FULL_TIME: 	noOfHrs = 8;
     						System.out.println("Employee is Full Time");
     						break;
-    	case IS_PART_TIME:	noOfHrs = 4;
+    		case IS_PART_TIME:	noOfHrs = 4;
     						System.out.println("Employee is Part Time");
     						break;
-    	default:			System.out.println("Employee is Absent");
+    		default:			System.out.println("Employee is Absent");
+    		}
+    		final int empWage= noOfHrs*PER_HOUR_WAGE;
+    		totalWage+=empWage;
     	}
-    	empWage= noOfHrs*PER_HOUR_WAGE;
-        System.out.println( "Employee's wage for the day :" + empWage);
+        System.out.println( "Employee's wage for the month :" + totalWage);
     }
 }
