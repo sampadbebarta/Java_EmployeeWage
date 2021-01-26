697b711431263e2379838a16e42e9ae2576b8d71
Sampad <bebartasampad@gmail.com>
2021-01-26 19:13:41 +0530

    Employee Wage of multiple companies using Interface

---
+++ CompanyEmpWage.java
@@ -0,0 +1,24 @@
+class CompanyEmpWage {
+
+		final String company;
+		final int empRatePerHour;
+		final int numOfWorkingDays;
+		final int maxHoursPerMonth;
+		int totalEmpWage;
+
+		public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+			this.company = company;
+			this.empRatePerHour = empRatePerHour;
+			this.numOfWorkingDays = numOfWorkingDays;
+			this.maxHoursPerMonth = maxHoursPerMonth;
+		}
+
+		public void setTotalEmpWage(int totalEmpWage) {
+			this.totalEmpWage = totalEmpWage;
+		}
+
+		public String toString() {
+			return "Total Emp Wage for Company "+ company + " is: "+totalEmpWage ;
+		}
+
+	}
---
+++ IComputeEmpWage.java
@@ -0,0 +1,5 @@
+public interface IComputeEmpWage {
+	public void computeEmpWage();
+	public void addCompanyEmpWage(String company, int empRatePerHour,
+					int numOfWorkingDays, int maxHoursPerMonth) ;
+}
